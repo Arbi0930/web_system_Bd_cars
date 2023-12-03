7a536284bbe4d6223941ba1c745c4f1c67114c50
Arbi0930 <mbsavage0930@gmail.com>
2023-12-04 01:48:48 +0800

    last

--- client-main/src/Component/Home/Subcribe/Subcribe.js
+++ client-main/src/Component/Home/Subcribe/Subcribe.js
@@ -5,11 +5,11 @@ const Subcribe = () => {
         <div>
             <section className="newsletter"> 
 
-            <h3>subscribe for latest updates</h3>
+            <h3>Шинэ мэдээллийг шууд авах</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, suscipit.</p>
     <form action="">
-        <input className='EMAIL' type="email" placeholder="enter your email"/>
-        <input className='SUB' type="submit" value="subscribe"/>
+        <input className='EMAIL' type="email" placeholder="И-Мэйл авах"/>
+        <input className='SUB' type="submit" value="бүртгүүлэх"/>
    </form>
                 
             </section>
